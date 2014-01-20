1a4e706c8629e14353c2e0a09ff886c9a46b5c0c
savin <mafei.198@gmail.com>
2014-01-20 11:55:28 +0800

    update

--- include/gproc_macros.hrl
+++ include/gproc_macros.hrl
@@ -1,4 +1,12 @@
--define(GPROC_KEY(Name), {n, g, Name}).
--define(GET_PID(Name), gproc:lookup_global_name(Name)).
--define(REG_PID(Name), gproc:add_global_name(Name)). %% Only can reg yourself.
--define(UNREG(Name), gproc:unreg(?GPROC_KEY(Name)).
+% Global gproc
+%-define(GPROC_KEY(Name), {n, g, Name}).
+%-define(GET_PID(Name), gproc:lookup_global_name(Name)).
+%-define(REG_PID(Name), gproc:add_global_name(Name)). %% Only can reg yourself.
+%-define(UNREG(Name), gproc:unreg(?GPROC_KEY(Name))).
+
+% local gproc
+-define(GPROC_KEY(Name), {n, l, Name}).
+-define(GET_PID(Name), gproc:where(?GPROC_KEY(Name))).
+-define(REG_PID(Name), gproc:reg(?GPROC_KEY(Name))). %% Only can reg yourself.
+-define(UNREG(Name), gproc:unreg(?GPROC_KEY(Name))).
+
--- src/game_server.erl
+++ src/game_server.erl
@@ -11,10 +11,8 @@ start() ->
     io:format("Game Server Starting~n"),
     ok = lager:start(), %% Logger
     ok = application:start(sync), %% Hot reload code
-    ok = application:start(bson), %% MongoDB dependency
     ok = application:start(gproc), %% Process dictionary
     ok = application:start(yamerl), %% yaml loader and writer
     ok = application:start(crypto),
-    ok = application:start(mongodb), %% MongoDB
-    ok = application:start(mongrel), %% MongoDB sql wrapper
+    ok = application:start(emysql), %% Mysql
     ok = application:start(game_server). %% Game Server
