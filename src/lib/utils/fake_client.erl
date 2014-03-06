31b1d070e6b69cd61fbd07a657c44e67613290af
savin <mafei.198@gmail.com>
2014-03-06 17:44:27 +0800

    update

--- src/lib/utils/fake_client.erl
+++ src/lib/utils/fake_client.erl
@@ -28,7 +28,7 @@
 
 
 login() ->
-    io:format("--------------Fake Client called login!~n"),
+    io:format("Fake Client called login!~n"),
     SomeHostInNet = "localhost", % to make it runnable on one machine
     {ok, Sock} = gen_tcp:connect(SomeHostInNet, 5555,
                                  [{active, false}, {packet, 2}]),
