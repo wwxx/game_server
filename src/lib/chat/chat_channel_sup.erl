507e84ad43b820b088c396c40e30c64ad9a138c2
savin <mafei.198@gmail.com>
2014-03-13 10:47:35 +0800

    update

--- src/lib/chat/chat_channel_sup.erl
+++ src/lib/chat/chat_channel_sup.erl
@@ -42,14 +42,6 @@
 %%%===================================================================
 %%% API functions
 %%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc
-%% Starts the supervisor
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
-%% @end
-%%--------------------------------------------------------------------
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
@@ -60,19 +52,6 @@ start_child(Args) ->
 %%% Supervisor callbacks
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Whenever a supervisor is started using supervisor:start_link/[2,3],
-%% this function is called by the new process to find out about
-%% restart strategy, maximum restart frequency and child
-%% specifications.
-%%
-%% @spec init(Args) -> {ok, {SupFlags, [ChildSpec]}} |
-%%                     ignore |
-%%                     {error, Reason}
-%% @end
-%%--------------------------------------------------------------------
 init([]) ->
     {ok, {{simple_one_for_one, 5, 10}, [?CHILD(undefined, player, worker, [])]}}.
 
