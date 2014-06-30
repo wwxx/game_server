cbb1e872e333e7c5eeb9d43cb08cf9f5edcbe7d7
savin <mafei.198@gmail.com>
2014-06-30 17:00:13 +0800

    update

--- src/utils/for.erl
+++ src/utils/for.erl
@@ -1,11 +1,15 @@
 -module(for).
 
--export([times/2, each/2, upto/3, upto/4]).
+-export([times/2, times/3, each/2, upto/3, upto/4]).
 
 times(0, _) -> ok;
 times(N, Fun) ->
     Fun(),
     times(N - 1, Fun).
+
+times(_Fun, Acc, 0) -> Acc;
+times(Fun, Acc, N) ->
+    times(Fun, Fun(Acc), N - 1).
 
 each([], _) -> ok;
 each([Item|List], Fun) ->
