0c84c3e5036c56ec474bb4ac3438dbee5a4e1792
Savin <mafei.198@gmail.com>
2014-08-06 10:08:55 +0800

    update

--- src/utils/mail.erl
+++ src/utils/mail.erl
@@ -2,12 +2,22 @@
 
 -export([send/4, exception_notify/1]).
 
--define(DEVELOPER_EMAILS, "mafei.198@gmail.com").
+-define(DEVELOPER_EMAILS, ["mafei.198@gmail.com", "abtree@qq.com"]).
 
 exception_notify(ExceptionMsg) ->
     {ok, From} = inet:gethostname(),
     send(From, ?DEVELOPER_EMAILS, "Server_Exception", ExceptionMsg).
 
-send(From, To, Subject, Body) ->
-    Content = io_lib:format("Subject: ~s \r\nFrom: ~s \r\nTo: ~s \r\n\r\n~s", [Subject, From, To, Body]), 
-    gen_smtp_client:send({From, [To], Content}, [{relay, "localhost"}, {port, 25}]).
+send(From, Receivers, Subject, Body) ->
+    To = lists:foldl(fun(Receiver, Acc) ->
+                         if
+                             Acc =:= "" ->
+                                 Receiver;
+                             true ->
+                                 lists:concat([Acc, ", ", Receiver])
+                         end
+                         
+                     end, "", Receivers),
+    Content = io_lib:format("Subject: ~s \r\nFrom: ~s \r\nTo: ~s \r\n\r\n~s", 
+                            [Subject, From, To, Body]), 
+    gen_smtp_client:send({From, Receivers, Content}, [{relay, "localhost"}, {port, 25}]).
