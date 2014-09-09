ba556bf87396b9876468aee6d390536f2bf2f99d
Savin <mafei.198@gmail.com>
2014-09-09 10:22:01 +0800

    update

--- src/game_connection.erl
+++ src/game_connection.erl
@@ -306,6 +306,6 @@ pack_response_data(RequestId, Data) ->
 
 send_socket_data(Transport, Socket, PureData) ->
     CipherData = secure:encrypt(?AES_KEY, ?AES_IVEC, PureData),
-    % error_logger:info_msg("Socket Data Length: ~p~n", [erlang:byte_size(CipherData)]),
-    % error_logger:info_msg("Socket Data: ~p~n", [CipherData]),
+    error_logger:info_msg("Socket Data Length: ~p~n", [erlang:byte_size(CipherData)]),
+    error_logger:info_msg("Socket Data: ~p~n", [CipherData]),
     Transport:send(Socket, CipherData).
