dee589ed1a670c1efb3a78ef42c4ed511c71bc45
Savin <mafei.198@gmail.com>
2014-09-11 15:26:00 +0800

    update

--- src/utils/mail.erl
+++ src/utils/mail.erl
@@ -5,7 +5,13 @@
 -define(DEVELOPER_EMAILS, ["mafei.198@gmail.com", "abtree123@gmail.com"]).
 
 exception_notify(ExceptionMsg) ->
-    {ok, From} = inet:gethostname(),
+    From = try get_ip() of
+               IP -> IP
+           catch
+               _:_ ->
+                   {ok, Hostname} = inet:gethostname(),
+                   Hostname
+           end,
     send(From, ?DEVELOPER_EMAILS, "Server_Exception", ExceptionMsg).
 
 send(From, Receivers, Subject, Body) ->
@@ -21,3 +27,7 @@ send(From, Receivers, Subject, Body) ->
     Content = io_lib:format("Subject: ~s \r\nFrom: ~s \r\nTo: ~s \r\n\r\n~s", 
                             [Subject, From, To, Body]), 
     gen_smtp_client:send({From, Receivers, Content}, [{relay, "localhost"}, {port, 25}]).
+
+get_ip() ->
+    {ok, [{IP, _, _}|_]} = inet:getif(),
+    IP.
