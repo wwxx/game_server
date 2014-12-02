a2eb3493b96e667effea3e825ebf65b76c23e289
Savin <mafei.198@gmail.com>
2014-12-02 10:57:48 +0800

    update

--- deps/player_server/src/memory_data_manager/model.erl
+++ deps/player_server/src/memory_data_manager/model.erl
@@ -35,7 +35,6 @@
          create/1,
          create/2,
          count/1,
-         sql/2,
          get_persist_all_sql/0,
          persist_all/0]).
 
@@ -234,49 +233,6 @@ create(Record, load) ->
     update_status(Table, Id, ?MODEL_ORIGIN),
     put({Table, Id}, Record).
 
-generate_persist_sql(Table) ->
-    StatusIdList = id_status_list(Table),
-    DeleteIdList = delete_status_list(Table),
-    if
-        StatusIdList =:= [] andalso DeleteIdList =:= [] -> <<>>;
-        true ->
-            case sqls(Table, StatusIdList ++ DeleteIdList) of
-                [] -> <<>>;
-                Sqls -> binary_string:join(Sqls, <<";">>)
-            end
-    end.
-
-persist_all() ->
-    try do_persist_all() of
-        Result -> 
-            Tables = all_loaded_tables(),
-            reset_tables_status(Tables),
-            Result
-    catch
-        Type:Msg ->
-            exception:notify(Type, Msg)
-    end.
-
-do_persist_all() ->
-    case get_persist_all_sql() of
-        <<>> -> do_nothing;
-        JoinedSql -> execute_with_procedure(JoinedSql)
-    end.
-
-get_persist_all_sql() ->
-    Tables = all_loaded_tables(),
-    PlayerID = get(player_id),
-    logger:info("PERSIST FOR: ~p~n", [PlayerID]),
-    Sqls = lists:foldl(fun(Table, Result) ->
-        case generate_persist_sql(Table) of
-            <<>> -> Result;
-            Sql -> 
-                logger:info("[~p] Table: ~p SQL: ~p~n", [PlayerID, Table, Sql]),
-                [Sql|Result]
-        end
-    end, [], Tables),
-    binary_string:join(Sqls, <<";">>).
-
 reset_tables_status(Tables) ->
     lists:foreach(fun reset_status/1, Tables).
 
@@ -434,41 +390,6 @@ delete_status_list(Table) ->
         IdList -> IdList
     end.
 
-sqls(Table, IdList) ->
-    lists:foldl(fun
-        ({Id, Status}, Result) ->
-            if
-                Status =:= ?MODEL_ORIGIN ->
-                    Result;
-                Status =:= ?MODEL_CREATE orelse Status =:= ?MODEL_UPDATE ->
-                    Rec = get({Table, Id}),
-                    [sql(Rec, Status)|Result];
-                Status =:= ?MODEL_DELETE ->
-                    [sql({Table, Id}, Status)|Result]
-            end
-    end, [], IdList).
-
-sql(Rec, ?MODEL_CREATE) ->
-    {Table, Fields, Values} = rec_info(Rec),
-    db_fmt:format("INSERT INTO `~s` (~s) VALUES (~s)", 
-                  [Table, join_fields(Fields), join_values(Values)]);
-sql(Rec, ?MODEL_UPDATE) ->
-    {Table, Fields, Values} = rec_info(Rec),
-    Uuid = hd(Values),
-    db_fmt:format("UPDATE `~s` SET ~s WHERE `uuid` = ~s", 
-                  [Table, db_fmt:map(Fields, Values), db_fmt:encode(Uuid)]);
-sql({Table, Uuid}, ?MODEL_DELETE) ->
-    db_fmt:format("DELETE FROM `~s` WHERE `uuid` = ~s", 
-                  [Table, db_fmt:encode(Uuid)]).
-
-rec_info(Rec) ->
-    [Table|Values] = tuple_to_list(Rec),
-    Fields = record_mapper:get_mapping(Table),
-    Module = list_to_atom(atom_to_list(Table) ++ "_model"),
-    Rule = proplists:get_value(serialize, Module:module_info(attributes)),
-    SerializedValues = serialize(Values, Fields, Rule),
-    {Table, Fields, SerializedValues}.
-
 add_to_delete_list(Table, Id) ->
     List = get({Table, deleteIdList}),
     if
@@ -490,34 +411,3 @@ update_record([RecordValue|RecordValues], [ModifierValue|ModifierValues], Result
     update_record(RecordValues, ModifierValues, [RecordValue|Result]);
 update_record([_RecordValue|RecordValues], [ModifierValue|ModifierValues], Result)->
     update_record(RecordValues, ModifierValues, [ModifierValue|Result]).
-
-serialize(Values, _Fields, undefined) -> Values;
-serialize(Values, Fields, Rule) ->
-    serialize(Values, Fields, Rule, []).
-
-serialize([], [], _Rule, Result) ->
-    lists:reverse(Result);
-serialize([Value|Values], [Field|Fields], Rule, Result) ->
-    case proplists:get_bool(Field, Rule) of
-        true when Value =/= undefined -> 
-            Data = term_to_binary(Value),
-            SerializedValue = base64:encode(Data),
-            serialize(Values, Fields, Rule, [SerializedValue|Result]);
-        _ ->
-            serialize(Values, Fields, Rule, [Value|Result])
-    end.
-
-join_fields(Fields) ->
-    join_fields(Fields, []).
-join_fields([], Result) ->
-    binary_string:join(lists:reverse(Result), <<", ">>);
-join_fields([Field|Fields], Result) ->
-    NewField = list_to_binary([<<"`">>, atom_to_binary(Field, utf8), <<"`">>]),
-    join_fields(Fields, [NewField|Result]).
-
-join_values(Values) ->
-    join_values(Values, []).
-join_values([], Result) ->
-    binary_string:join(lists:reverse(Result), <<", ">>);
-join_values([Value|Values], Result) -> 
-    join_values(Values, [db_fmt:encode(Value)|Result]).
--- deps/player_server/src/memory_data_manager/player_data.erl
+++ deps/player_server/src/memory_data_manager/player_data.erl
@@ -41,6 +41,7 @@
          count_all/2,
 
          get_all_recs/2,
+         get_loaded_tables/1,
          ensure_load_data/2,
          update_rec_status/4,
          delete_rec_status/4,
@@ -156,10 +157,13 @@ get_all_recs(PlayerID, Table) ->
     end, [], ets:lookup(?DATA_STATUS, Key)).
 
 update_rec_status(PlayerID, Table, Id, Status) ->
-    ets:insert(?DATA_STATUS, {PlayerID, {Table, Id, Status}}).
+    ets:insert(?DATA_STATUS, {{PlayerID, Table}, {Id, Status}}).
 
 delete_rec_status(PlayerID, Table, Id, Status) ->
-    ets:delete_object(?DATA_STATUS, {PlayerID, {Table, Id, Status}}).
+    ets:delete_object(?DATA_STATUS, {{PlayerID, Table}, {Id, Status}}).
+
+get_rec_status(PlayerID, Table) ->
+    ets:lookup(?DATA_STATUS, {PlayerID, Table}).
 
 update_rec(Table, Rec) ->
     EtsTab = ets_tab_name(Table),
@@ -192,10 +196,14 @@ set_loaded(PlayerID, ModelName, Loaded) ->
     Key = {PlayerID, ModelName, loaded},
     if
         Loaded =:= true ->
-            ets:insert(?DATA_LOADED, {Key, true});
+            ets:insert(?DATA_LOADED, {Key, true}),
+            ets:insert(?DATA_LOADED, {{PlayerID, loaded_tables}, ModelName});
         true ->
             ets:delete(?DATA_LOADED, Key)
     end.
+
+get_loaded_tables(PlayerID) ->
+    ets:lookup(?DATA_LOADED, {PlayerID, loaded_tables}).
 
 insert_recs(PlayerID, Recs, Module, EtsTab) ->
     case lists:keyfind(serialize, 1, Module:module_info(attributes)) of
@@ -236,3 +244,120 @@ deserialize([Value|Values], [Field|Fields], Rule, Result) ->
 
 ets_tab_name(Table) ->
     list_to_atom(atom_to_list(Table) ++ "_ets_table").
+
+persist_for_player(PlayerID) ->
+    lists:foldl(fun({_, Table}, Result) ->
+        RecStatus = get_rec_status(PlayerID, Table),
+        case sqls(Table, RecStatus) of
+            [] -> <<>>;
+            Sqls -> binary_string:join(Sqls, <<";">>)
+        end
+    end, [], get_loaded_tables(PlayerID)).
+
+persist_for_player(PlayerID) ->
+    try do_persist_for_player(PlayerID) of
+        Result -> 
+            Tables = all_loaded_tables(),
+            reset_tables_status(Tables),
+            Result
+    catch
+        Type:Msg ->
+            exception:notify(Type, Msg)
+    end.
+
+do_persist_for_player(PlayerID) ->
+    case get_persist_all_sql() of
+        <<>> -> do_nothing;
+        JoinedSql -> execute_with_procedure(JoinedSql)
+    end.
+
+get_persist_all_sql() ->
+    PlayerID = get(player_id),
+    logger:info("PERSIST FOR: ~p~n", [PlayerID]),
+    Sqls = lists:foldl(fun({_, Table}, Result) ->
+        case generate_persist_sql(Table) of
+            <<>> -> Result;
+            Sql -> 
+                logger:info("[~p] Table: ~p SQL: ~p~n", [PlayerID, Table, Sql]),
+                [Sql|Result]
+        end
+    end, [], player_data:get_loaded_tables(PlayerID)),
+    binary_string:join(Sqls, <<";">>).
+
+generate_persist_sql(Table) ->
+    StatusIdList = id_status_list(Table),
+    DeleteIdList = delete_status_list(Table),
+    if
+        StatusIdList =:= [] andalso DeleteIdList =:= [] -> <<>>;
+        true ->
+            case sqls(Table, StatusIdList ++ DeleteIdList) of
+                [] -> <<>>;
+                Sqls -> binary_string:join(Sqls, <<";">>)
+            end
+    end.
+
+sqls(Table, IdList) ->
+    lists:foldl(fun
+        ({Id, Status}, Result) ->
+            if
+                Status =:= ?MODEL_ORIGIN ->
+                    Result;
+                Status =:= ?MODEL_CREATE orelse Status =:= ?MODEL_UPDATE ->
+                    Rec = get({Table, Id}),
+                    [sql(Rec, Status)|Result];
+                Status =:= ?MODEL_DELETE ->
+                    [sql({Table, Id}, Status)|Result]
+            end
+    end, [], IdList).
+
+sql(Rec, ?MODEL_CREATE) ->
+    {Table, Fields, Values} = rec_info(Rec),
+    db_fmt:format("INSERT INTO `~s` (~s) VALUES (~s)", 
+                  [Table, join_fields(Fields), join_values(Values)]);
+sql(Rec, ?MODEL_UPDATE) ->
+    {Table, Fields, Values} = rec_info(Rec),
+    Uuid = hd(Values),
+    db_fmt:format("UPDATE `~s` SET ~s WHERE `uuid` = ~s", 
+                  [Table, db_fmt:map(Fields, Values), db_fmt:encode(Uuid)]);
+sql({Table, Uuid}, ?MODEL_DELETE) ->
+    db_fmt:format("DELETE FROM `~s` WHERE `uuid` = ~s", 
+                  [Table, db_fmt:encode(Uuid)]).
+
+rec_info(Rec) ->
+    [Table|Values] = tuple_to_list(Rec),
+    Fields = record_mapper:get_mapping(Table),
+    Module = list_to_atom(atom_to_list(Table) ++ "_model"),
+    Rule = proplists:get_value(serialize, Module:module_info(attributes)),
+    SerializedValues = serialize(Values, Fields, Rule),
+    {Table, Fields, SerializedValues}.
+
+serialize(Values, _Fields, undefined) -> Values;
+serialize(Values, Fields, Rule) ->
+    serialize(Values, Fields, Rule, []).
+
+serialize([], [], _Rule, Result) ->
+    lists:reverse(Result);
+serialize([Value|Values], [Field|Fields], Rule, Result) ->
+    case proplists:get_bool(Field, Rule) of
+        true when Value =/= undefined -> 
+            Data = term_to_binary(Value),
+            SerializedValue = base64:encode(Data),
+            serialize(Values, Fields, Rule, [SerializedValue|Result]);
+        _ ->
+            serialize(Values, Fields, Rule, [Value|Result])
+    end.
+
+join_fields(Fields) ->
+    join_fields(Fields, []).
+join_fields([], Result) ->
+    binary_string:join(lists:reverse(Result), <<", ">>);
+join_fields([Field|Fields], Result) ->
+    NewField = list_to_binary([<<"`">>, atom_to_binary(Field, utf8), <<"`">>]),
+    join_fields(Fields, [NewField|Result]).
+
+join_values(Values) ->
+    join_values(Values, []).
+join_values([], Result) ->
+    binary_string:join(lists:reverse(Result), <<", ">>);
+join_values([Value|Values], Result) -> 
+    join_values(Values, [db_fmt:encode(Value)|Result]).
